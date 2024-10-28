d7430ddd1c0e298e176884faf6f28b9363072c97
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-10-28 22:16:14 +0530

    trying to manage role dropdown

--- JS/singlepage.js
+++ JS/singlepage.js
@@ -1,8 +1,14 @@
 $('#welcomePage').css("display", "block");
 $('#signin').css("display", "none");
+$("#signup").css("display", "none");
 
 $("#btnSignin").on('click', function() {
     $('#welcomePage').css("display", "none");
     $('#signin').css("display", "block");
+    $("#signup").css("display", "none");
 });
-
+$("#text").on('click', function() {
+    $('#welcomePage').css("display", "none");
+    $('#signin').css("display", "none");
+    $("#signup").css("display", "block");
+});
