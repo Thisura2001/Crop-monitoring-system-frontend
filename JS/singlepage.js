28612309df945fc6b17e15d1cbaca7f3961dac12
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-10-28 16:01:08 +0530

    start signin page

--- JS/singlepage.js
+++ JS/singlepage.js
@@ -1,1 +1,8 @@
-$
+$('#welcomePage').css("display", "block");
+$('#signin').css("display", "none");
+
+$("#btnSignin").on('click', function() {
+    $('#welcomePage').css("display", "none");
+    $('#signin').css("display", "block");
+});
+
