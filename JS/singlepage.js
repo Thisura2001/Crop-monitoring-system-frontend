5a01ebda453d078efc70454c4dbeb3fa0f748d02
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-10-29 15:23:59 +0530

    add nav bar to the dashboard

--- JS/singlepage.js
+++ JS/singlepage.js
@@ -1,14 +1,26 @@
 $('#welcomePage').css("display", "block");
 $('#signin').css("display", "none");
 $("#signup").css("display", "none");
+$("#dashboard").css("display", "none");
 
 $("#btnSignin").on('click', function() {
     $('#welcomePage').css("display", "none");
     $('#signin').css("display", "block");
     $("#signup").css("display", "none");
+    $("#dashboard").css("display", "none");
+});
+$("#btnlogin").on('click', function() {
+    $('#welcomePage').css("display", "none");
+    $('#signin').css("display", "none");
+    $("#signup").css("display", "none");
+    $("#dashboard").css("display", "block");
 });
 $("#text").on('click', function() {
     $('#welcomePage').css("display", "none");
     $('#signin').css("display", "none");
     $("#signup").css("display", "block");
+    $("#dashboard").css("display", "none");
 });
+function selectRole(role) {
+    document.getElementById('roleDropdown').innerText = role;
+}
