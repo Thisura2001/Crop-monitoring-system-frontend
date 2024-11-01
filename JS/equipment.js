292a0862e725cf4080e1c26044593d7eae72f91c
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-11-01 09:38:11 +0530

    equipment structure done

---
+++ JS/equipment.js
@@ -0,0 +1,7 @@
+document.getElementById("addEquipmentBtn").addEventListener("click", function() {
+    document.getElementById("equipmentFormCard").style.display = "block";
+});
+
+function closeEquipmentForm() {
+    document.getElementById("equipmentFormCard").style.display = "none";
+}
