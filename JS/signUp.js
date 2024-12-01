aceb65b6086f4f9fdcf14baa8f811e052c21c50c
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-12-01 12:13:55 +0530

    signup role updated

--- JS/signUp.js
+++ JS/signUp.js
@@ -1,8 +1,10 @@
-function selectRole(role) {
-    document.getElementById("roleDropdown").textContent = role;
-}
 
 $("#backBtn").on('click', function() {
     $("#signin").css( "display", "block");
     $("#signup").css( "display", "none");
+})
+$("#btnSignUp").on('click', function() {
+    let email = $("#email1").val();
+    let password = $("#password1").val();
+    let role =$("#roleDropdown").text();
 })
